8eabd44fd54cd8ebfda6ca47db9bf76f8c4dd2e9
Mario Elvio <juka_mebaj@hotmail.com>
2022-11-27 02:46:29 -0300

    feat: front-end (update email and update password).

--- front-end/src/components/modal/ModalUpdateEmail/index.tsx
+++ front-end/src/components/modal/ModalUpdateEmail/index.tsx
@@ -1,6 +1,16 @@
+import { useState, FormEvent, useContext } from "react";
 import * as C from "./styles";
 import { Modal } from "../Modal";
+import { toast } from "react-toastify";
+
+import { theme } from "../../../styles/Theme";
+
 import FormTitle from "../../title/FormTitle";
+import { PrimaryButton } from "../../button/PrimaryButton";
+import { Input } from "../../TextField";
+import { AuthContext } from "../../../contexts/AuthContext";
+
+import { setupAPIClient } from "../../../services/api";
 
 interface ModalUpdateEmailProps {
   isOpened: boolean;
@@ -8,11 +18,88 @@ interface ModalUpdateEmailProps {
 }
 
 export default function ModalUpdateEmail({ isOpened, toggleModal }: ModalUpdateEmailProps) {
+  const { user } = useContext(AuthContext);
+
+  const [ isLoading, setIsLoading ] = useState<boolean>(false);
+  const [ email, setEmail ] = useState<string>('');
+
+  async function handleUpdateEmail(event: FormEvent) {
+    event.preventDefault();
+
+    if (email === '') {
+      toast.error('Por favor, preencha o novo e-mail!');
+      return;
+    }
+
+    setIsLoading(true);
+
+    const apiClient = setupAPIClient();
+
+    await apiClient.put('/user', {
+        id_user: user.id,
+        email: email
+    })
+    .then(() => {
+      setEmail('');
+      toggleModal();
+      toast.success('E-mail salvo com sucesso!');
+    })
+    .catch((error) => {
+      console.log(error);
+      toast.error('Não foi possível salvar o novo e-mail!');
+    });
+
+    setIsLoading(false);
+  }
+
+  function handleCancel() {
+    setEmail('');
+    toggleModal();
+  }
+  
   return (
-    <Modal toggleModal={toggleModal} isOpened={isOpened} >
-      <C.Form>
+    <Modal
+      toggleModal={toggleModal}
+      isOpened={isOpened}
+      isLoading={isLoading}
+    >
+      <C.Form onSubmit={handleUpdateEmail}>
         <FormTitle>Alterar E-mail</FormTitle>
-        <button onClick={toggleModal}>fechar</button>
+
+        <C.ContainerForm>
+          <div className="container-input">
+            <label htmlFor="email">E-mail:</label>
+
+            <Input
+              type="email"
+              id="email"
+              name="email"
+              aria-label="E-mail"
+              placeholder="Digite seu novo e-mail"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              required
+            />
+          </div>
+
+          <div className="container-button">
+            <PrimaryButton
+              type="button"
+              onClick={handleCancel}
+              backgroundColor={theme.colors.coralRed}
+              color={theme.colors.white}
+            >
+              Cancelar
+            </PrimaryButton>
+            <PrimaryButton
+              type="submit"
+              backgroundColor={theme.colors.aquamarine}
+              color={theme.colors.black}
+            >
+              Salvar
+            </PrimaryButton>
+          </div>
+        </C.ContainerForm>
       </C.Form>
     </Modal>
   )
--- front-end/src/components/modal/ModalUpdatePassword/index.tsx
+++ front-end/src/components/modal/ModalUpdatePassword/index.tsx
@@ -1,6 +1,16 @@
+import { useState, FormEvent, useContext } from "react";
 import * as C from "./styles";
 import { Modal } from "../Modal";
+import { toast } from "react-toastify";
+
+import { theme } from "../../../styles/Theme";
+
 import FormTitle from "../../title/FormTitle";
+import { PrimaryButton } from "../../button/PrimaryButton";
+import { Input } from "../../TextField";
+import { AuthContext } from "../../../contexts/AuthContext";
+
+import { setupAPIClient } from "../../../services/api";
 
 interface ModalUpdatePasswordProps {
   isOpened: boolean;
@@ -8,11 +18,116 @@ interface ModalUpdatePasswordProps {
 }
 
 export default function ModalUpdatePassword({ isOpened, toggleModal }: ModalUpdatePasswordProps) {
+  const { user } = useContext(AuthContext);
+
+  const [ isLoading, setIsLoading ] = useState<boolean>(false);
+  const [ password, setPassword ] = useState<string>('');
+  const [ passwordAux, setPasswordAux ] = useState<string>('');
+
+  async function handleUpdatePassword(event: FormEvent) {
+    event.preventDefault();
+
+    if (password === '') {
+      toast.error('Por favor, preencha o campo nova senha!');
+      return;
+    }
+
+    if (passwordAux === '') {
+      toast.error('Por favor, preencha o campo repetir nova senha!');
+      return;
+    }
+
+    if (password !== passwordAux) {
+      toast.error('Senhas estão divergentes!');
+      return;
+    }
+
+    setIsLoading(true);
+
+    const apiClient = setupAPIClient();
+
+    await apiClient.put('/user', {
+        id_user: user.id,
+        password: password
+    })
+    .then(() => {
+      setPassword('');
+      setPasswordAux('');
+      toggleModal();
+      toast.success('Senha salva com sucesso!');
+    })
+    .catch((error) => {
+      console.log(error);
+      toast.error('Não foi possível salvar a nova senha!');
+    });
+
+    setIsLoading(false);
+  }
+
+  function handleCancel() {
+    setPassword('');
+    setPasswordAux('');
+    toggleModal();
+  }
+  
   return (
-    <Modal toggleModal={toggleModal} isOpened={isOpened} >
-      <C.Form>
+    <Modal
+      toggleModal={toggleModal}
+      isOpened={isOpened}
+      isLoading={isLoading}
+    >
+      <C.Form onSubmit={handleUpdatePassword}>
         <FormTitle>Alterar Senha</FormTitle>
-        <button onClick={toggleModal}>fechar</button>
+
+        <C.ContainerForm>
+          <div className="container-input">
+            <label htmlFor="username">Nova senha:</label>
+
+            <Input
+              type="password"
+              id="username"
+              name="username"
+              aria-label="Usuário"
+              placeholder="Digite seu novo usuário"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              required
+            />
+          </div>
+
+          <div className="container-input">
+            <label htmlFor="username">Repetir nova senha:</label>
+
+            <Input
+              type="password"
+              id="username"
+              name="username"
+              aria-label="Usuário"
+              placeholder="Digite seu novo usuário"
+              value={passwordAux}
+              onChange={(e) => setPasswordAux(e.target.value)}
+              required
+            />
+          </div>
+
+          <div className="container-button">
+            <PrimaryButton
+              type="button"
+              onClick={handleCancel}
+              backgroundColor={theme.colors.coralRed}
+              color={theme.colors.white}
+            >
+              Cancelar
+            </PrimaryButton>
+            <PrimaryButton
+              type="submit"
+              backgroundColor={theme.colors.aquamarine}
+              color={theme.colors.black}
+            >
+              Salvar
+            </PrimaryButton>
+          </div>
+        </C.ContainerForm>
       </C.Form>
     </Modal>
   )
